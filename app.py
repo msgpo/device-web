d5e84f970491273dfb794f700cea33d47925cab1
Yihui Xiong <xiongyihui3@gmail.com>
2019-08-06 16:49:11 +0800

    initial

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+from datetime import timedelta
+
+#https://github.com/jupyterlab/jupyterlab/blob/master/examples/app/main.py
+# from jupyterlab_server import LabServerApp
+# LabServerApp.launch_instance()
+
+app = Flask(__name__)
+app.secret_key = 'any random string x'
+
+# app.permanent_session_lifetime = timedelta(minutes=1)
+
+
+apps = [
+    {'name': 'Music', 'url': '#', 'icon': 'music.svg'},
+    {'name': 'Home Assistant', 'url': '#', 'icon': 'home_assistant.svg'},
+    {'name': 'Google Assistant', 'url': '#', 'icon': 'google_assistant.svg'},
+    {'name': 'Amazon Alexa', 'url': '#', 'icon': 'alexa.svg'}
+]
+
+
+@app.route('/')
+def index():
+    user = session['user'] if 'user' in session else None
+    return render_template('index.html', user=user, apps=apps)
+
+@app.route('/terminal')
+def terminal():
+    url = 'http://{0}/?src=terminal'.format(request.host)
+    return redirect(url)
+
+@app.route('/jupyter')
+def jupyter():
+    if 'user' not in session:
+        return redirect(url_for('login', next='jupyter'))
+    else:
+        user = session['user']
+        # todo: start jupyter-lab and navigate to it
+        url = 'http://{0}/?src=jupyter'.format(request.host)
+        return redirect(url)
+    return render_template('login.html')
+
+@app.route('/music')
+def music():
+    url = 'http://{0}/?src=music'.format(request.host)
+    return redirect(url)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print(request.args)
+    print(request.method)
+    if request.method == 'POST':
+        session['user'] = request.form['user']
+        session.permanent = True
+        next = request.args['next'] if 'next' in request.args else 'index'
+        return redirect(url_for(next))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    # remove the user from the session if it is there
+    session.pop('user', None)
+    return redirect(url_for('index'))
