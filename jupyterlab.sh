6f3c3acb0321b079e7e8b14890f69bb1a55518d1
Yihui Xiong <yihui.xiong@hotmail.com>
2019-08-23 17:12:34 +0800

    listen on any ip

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -3,8 +3,4 @@
 [ -d ~/workspace ] || mkdir ~/workspace
 cd ~/workspace
 
-# IP=$(ip address | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
-# IP=127.0.0.1
-IP=$(ip address show wlan0 | sed -ne 's/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p')
-
-jupyter-lab  -y --no-browser --allow-root --ip ${IP} --notebook-dir ~/workspace
+jupyter-lab  -y --no-browser --allow-root --ip '' --notebook-dir ~/workspace
